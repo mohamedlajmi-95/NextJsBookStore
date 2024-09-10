9d0e0f345e3c1ed9b6625105e911be2568da528f
Mohamed Lajmi <mohamedlajmi1995@gmail.com>
2024-09-10 21:31:49 +0100

    v4

--- src/app/admin/Layout.js
+++ src/app/admin/Layout.js
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from ".@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import SideBar from "@/components/admin/SideBar";
 export default async function AdminLayout({ children }) {
--- src/components/client/Menu.jsx
+++ src/components/client/Menu.jsx
@@ -8,7 +8,18 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import HelpIcon from "@mui/icons-material/Help";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useSession, signIn, signOut } from "next-auth/react";
+
+import { useRouter } from "next/navigation";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useShoppingCart } from "use-shopping-cart";
+import CartModal from "./shoppingCart/CartModal";
+
+
 function Menu() {
+  const router = useRouter();
+  const { handleCartClick, cartCount } = useShoppingCart();
+
   const { data: session } = useSession();
   return (
     <>
@@ -31,15 +42,26 @@ function Menu() {
                 Se connecter
               </Nav.Link>
             )}
-            {/* <Nav.Link as={Link} href="/">
-              <AccountCircleIcon />
-              Se connecter
-            </Nav.Link> */}
+
+            <Nav.Link
+              onClick={() => router.push("/shoppingClient/cartProducts")}
+            >
+              <ShoppingBasketIcon style={{ color: "aqua" }} /> Shopping Cart
+            </Nav.Link>
+
             <Nav.Link as={Link} href="/">
               <HelpIcon />
               Aide
             </Nav.Link>
           </Nav>
+
+          <button className="relative" onClick={() => handleCartClick()}>
+            <ShoppingCartIcon color="warning" />
+            <div className="rounded-full flex justify-center items-center bg-red-500 text-xs text-white absolute w-6 h-5 bottom-6 -right-1">
+              {cartCount}
+            </div>
+          </button>
+          <CartModal />
         </Container>
       </Navbar>
     </>
